e5516fcdb79ebb66f8611d9f3c0a082d25745419
deethompson <devon.thompson0@gmail.com>
2019-09-25 16:23:41 -0400

    second commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,13 +2,19 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping ("/")
+
+
+    @RequestMapping  ("/")
         public String index2(){
             return "index2";
         }
-
+    @RequestMapping("/Welcome")
+    public String profile() {
+        return "Welcome";
+    }
     }
 
